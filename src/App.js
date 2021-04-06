91e51da6f5266ede96969bba28dff393992db85d
Adam Boczek <adam.boczek@nttdata.com>
2021-04-06 18:08:59 +0200

    add antd layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import { v4 as uuid } from 'uuid';
-import 'antd/dist/antd.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import { StateMaintenance } from './components/StateMaintenance';
+import 'antd/dist/antd.css';
 
-const CLIENT_ID = uuid();
+const { SubMenu } = Menu;
+const { Header, Content, Footer } = Layout;
 
 function App() {
 
   return (
     <>
-      <StateMaintenance />
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content">
+            <StateMaintenance />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>©2021 NTT DATA Serverless Demo</Footer>
+      </Layout>
     </>
   );
 }
